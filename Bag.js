3f96b57df2e97354cbf0ee0bd07778b3e5e834be
snovel ice <117592220+SnovelIce@users.noreply.github.com>
2024-07-01 07:53:00 +0300

    Create Bag.js

---
+++ Bag.js
@@ -0,0 +1,5 @@
+//                [[Inventory: items, currenct, pokeballs, etc...]]
+
+
+//                {gain pokeballs}
+App.game.pokeballs.gainPokeballs(GameConstants.Pokeball.Masterball, 1, false); // [1] Pokeball: what pokeball to give the trainer // [2] amount: how meny pokeballs to give, [3] ?purchase: did the trainer purchased the pokeball
